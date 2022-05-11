4b917ce31fcf7a7f6e3008f9df3f10097bf2f887
oraweechan <89260194+oraweechan@users.noreply.github.com>
2022-05-11 08:19:15 -0700

    Time: 153 ms (44.08%), Space: 47.9 MB (59.04%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,26 +3,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    if(s.length <= 1) return s.length;
-    
-    let longest = 0;
-    
-    for(let left = 0; left < s.length; left++) {
-      let seenChars = {}, currentLength = 0;
-
-      for(let right = left; right < s.length; right++) {
-        const currentChar = s[right];
-
-        if(!seenChars[currentChar]) {
-          currentLength++;
-          seenChars[currentChar] = true;
-          longest = Math.max(longest, currentLength);
-        } else {
-          break;
+    let seen = {}, left = 0, longest = 0;
+    for (let right=0; right<s.length; right++) {
+        let currentChar = s[right];
+        let prevChar = seen[currentChar];
+        if (prevChar >= left) {
+            left = prevChar + 1;
         }
-      }
+        seen[currentChar] = right;
+        longest = Math.max(longest, right-left+1)
     }
-    
     return longest;
 
     
